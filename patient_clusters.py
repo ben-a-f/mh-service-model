eb58737d7dffaecf8899f4a83d690b352a952be8
ben-a-f <112167390+ben-a-f@users.noreply.github.com>
2023-05-03 16:55:16 +0100

    Clustering Tests
    Fixed an issue where calculate_sse() wasn't actually calculating sse correctly.
    
    Added some inspection of the clustering tests in a Jupyter notebook.

--- patient_clusters.py
+++ patient_clusters.py
@@ -1,3 +1,8 @@
+# This file trials two scaling methods and four clustering algorithms on the synthetic patient data.
+# Scaling: RobustScaler(), StandardScaler()
+# Clustering: DBSCAN, k-Means, Agglomerative (Hierarchical), Gaussian Mixture Model
+# The results are outputted in .csv format and inspected in a separate script, cluster_model_inspection.py
+
 import pandas as pd
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.mixture import GaussianMixture
@@ -12,6 +17,9 @@ synthetic_patients = pd.read_csv("synthetic_patients.csv")
 synthetic_patients["EntryDate"] = pd.to_datetime(synthetic_patients["EntryDate"], format="%Y-%m-%d")
 synthetic_patients["EntryMonth"] = synthetic_patients["EntryDate"].dt.month
 
+# Note: After some testing, the StandardScaler() is less effective than the RobustScaler().
+# The sse scores are comparable but the silhouette scores are worse, suggesting that the optimal number of clusters
+# with either method is similar, but that the clusters defined with StandardScaler() data are less distinct.
 transform_cols = ["Age", "LoS", "DailyContacts", "EntryMonth"]
 scaler = RobustScaler()
 # scaler = StandardScaler()
@@ -22,17 +30,18 @@ scaled_data.columns = transform_cols
 # Function to calculate the Sum Squared Error (sse) of a given clustering.
 def calculate_sse(labels, data):
     centroids = []
-    for label in labels.unique():
-        centroid = np.array(np.mean(data[labels == label], axis=0))
-        centroids.append(centroid)
-    centroids = np.array(centroids)
-    sse = np.sum(np.array((data - centroids[labels])) ** 2)
+    sse = 0
+    for i in labels.unique():
+        cluster_points = np.array(data[labels == i])
+        center = np.array(np.mean(cluster_points, axis=0))
+        centroids.append(center)
+        sse += np.sum((cluster_points - center) ** 2)
     return sse
 
 # DBSCAN
 # Visually inspect various choices of min_samples, n.
 # Defines the min number of samples in a neighbourhood to define a core point.
-n = 6
+n = 8
 # Create distance matrix.
 dist = pd.DataFrame(squareform(pdist(scaled_data)),
                     columns=scaled_data.index,
@@ -43,8 +52,8 @@ fig, ax1 = plt.subplots()
 ax1.plot(nth_dist)
 
 # Apply algorithm for a range of parameters.
-min_samples = [2, 3, 4, 5, 6]
-eps = [0.95, 1.1, 1.3, 1.5, 1.6]
+min_samples = [2, 3, 4, 5, 6, 7, 8]
+eps = [0.95, 1.1, 1.3, 1.5, 1.6, 1.7, 1.7]
 num_clusters = []
 silhouette_scores = []
 sse_scores = []
@@ -67,7 +76,7 @@ results_DBSCAN["algorithm"] = "DBSCAN"
 
 # K-Means
 # Apply algorithm for a range of parameters.
-k_range = [2, 3, 4, 5, 6]
+k_range = [2, 3, 4, 5, 6, 7, 8]
 silhouette_scores = []
 sse_scores = []
 for k in k_range:
@@ -93,7 +102,7 @@ dendrogram(Z, ax=ax3)
 plt.show()
 
 # Apply algorithm for a range of parameters.
-max_clusters = [2, 3, 4, 5, 6]
+max_clusters = [2, 3, 4, 5, 6, 7, 8]
 silhouette_scores = []
 sse_scores = []
 for t in max_clusters:
@@ -109,7 +118,7 @@ results_agg["algorithm"] = "agglomerative"
 
 # Gaussian Mixture Models
 # Apply algorithm for a range of parameters.
-n_range = [2, 3, 4, 5, 6]
+n_range = [2, 3, 4, 5, 6, 7, 8]
 silhouette_scores = []
 sse_scores = []
 for n in n_range:
@@ -128,3 +137,5 @@ results_gmm["algorithm"] = "gmm"
 results = pd.concat([results_gmm, results_kMeans, results_agg, results_DBSCAN]).reset_index(drop=True)
 results["scaler"] = "robust"
 results.to_csv("robust_scaler_clustering.csv", index=False)
+# results["scaler"] = "standard"
+# results.to_csv("standard_scaler_clustering.csv", index=False)
