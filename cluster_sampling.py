bbc7220c0bfe7db107189e5adc7709d4b25d6012
ben-a-f <112167390+ben-a-f@users.noreply.github.com>
2023-05-23 12:15:38 +0100

    Single Cluster Sample
    Added functionality to generate a single sample from a specified cluster. I think this will be useful for the simpy model.

--- cluster_sampling.py
+++ cluster_sampling.py
@@ -24,9 +24,12 @@ lower_bounds = np.array([18, 0, 0])
 scaled_bounds = scaler.transform(lower_bounds.reshape(1, -1))
 # END MANUAL PARAMETERS
 
+# SECTION 1: Generating a whole synthetic dataset based on the input dataset.
+
 
 # Generate truncated multivariate normal samples.
-def truncated_mv_normal(mean, cov, size, lower_bounds, upper_bounds):
+def truncated_mv_normal(mean, cov, size, lower_bounds=np.array([-np.inf, -np.inf, -np.inf]),
+                    upper_bounds=np.array([np.inf, np.inf, np.inf])):
     samples = []
     while len(samples) < size:
         sample = np.random.multivariate_normal(mean=mean, cov=cov)
@@ -36,7 +39,7 @@ def truncated_mv_normal(mean, cov, size, lower_bounds, upper_bounds):
 
 
 # Generate total_n samples from the cluster model, using the model object kmeans.
-def cluster_samples(kmeans, data, total_n, lower_bounds=np.array([-np.inf, -np.inf, -np.inf]),
+def generate_synthetic_dataset(kmeans, data, total_n, lower_bounds=np.array([-np.inf, -np.inf, -np.inf]),
                     upper_bounds=np.array([np.inf, np.inf, np.inf])):
     new_samples = pd.DataFrame(columns=[i for s in [[x for x in data.columns], ["Label"]] for i in s])
     # Get cluster proportions
@@ -59,7 +62,32 @@ def cluster_samples(kmeans, data, total_n, lower_bounds=np.array([-np.inf, -np.i
 
 
 # Generate samples.
-scaled_samples = cluster_samples(km, scaled_data, 1000, scaled_bounds).reset_index()
+scaled_samples = generate_synthetic_dataset(km, scaled_data, 1000, scaled_bounds).reset_index()
 synthetic_samples = pd.DataFrame(scaler.inverse_transform(scaled_samples[transform_cols]))
 synthetic_samples.columns = transform_cols
 synthetic_samples = pd.concat([synthetic_samples, scaled_samples["Label"]], axis=1)
+
+# SECTION 2: Generating individual samples from a specified patient cluster.
+
+
+# Retrieve sample distribution parameters (so it only has to be done once).
+def get_sample_parameters(kmeans, data):
+    cluster_proportions = pd.Series(kmeans.labels_).value_counts() / np.sum(pd.Series(kmeans.labels_).value_counts())
+    cluster_centres = kmeans.cluster_centers_
+    cluster_covariances = []
+    for i in range(kmeans.n_clusters):
+        points = data.loc[kmeans.labels_ == i]
+        if len(points) == 1:
+            cov = np.eye(len(cluster_centres[i]))
+        else:
+            cov = np.cov(points.T)
+        cluster_covariances.append(cov)
+    cluster_covariances = np.array(cluster_covariances)
+    return cluster_proportions, cluster_centres, cluster_covariances
+
+
+props, centres, covs = get_sample_parameters(km, scaled_data)
+# To generate a sample from cluster i, we can now use truncated_mv_normal()
+target_cluster = np.random.choice(km.n_clusters, 1, p=props)[0]
+scaled_single = truncated_mv_normal(centres[target_cluster], covs[target_cluster], 1, scaled_bounds)
+synthetic_single = scaler.inverse_transform(scaled_single)
